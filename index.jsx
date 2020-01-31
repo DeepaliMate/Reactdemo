1527ce6c0957160e6cb9a2d84cd57a9e3a9b5a28
DeepaliMate <matedeepali48@gmail.com>
2020-01-31 16:59:18 +0100

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.min.css";
+import SideNav, {
+  Toggle,
+  Nav,
+  NavItem,
+  NavIcon,
+  NavText
+} from "@trendmicro/react-sidenav";
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import Home from "./Componets/side-navbar/comp1";
+
+ReactDOM.render(<Home />, document.getElementById("root"));
+serviceWorker.unregister();
